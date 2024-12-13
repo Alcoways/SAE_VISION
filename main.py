da2a3635b02cb8f58ee108108c1dfc940f7708fa
Tom Desalle <toto.desalle@gmail.com>
2024-12-13 17:25:06 +0100

    Methode 1 terminée

--- main.py
+++ main.py
@@ -4,24 +4,43 @@ import numpy as np
 import math
 import os
 import random
+import subprocess
 
-##################################
-executableopenscad="D:\OpenSCAD-2021.01-x86-64\openscad-2021.01\openscad.exe"
-d1=10
-d2=5
-l_table=1000
-l_mire=70
-l_coin=2
-mire=1
-u=random.randint(-l_mire//2+d1//2,l_mire//2-d1//2)
-v=random.randint(-l_mire//2+d1//2,l_mire//2-d1//2)
-c1="[160/255,119/255,109/255]"
-c2="[249/255,58/255,58/255]"
-chaine=f'" {executableopenscad}" -D d1={d1} -D d2={d2} -D u={u} -D v={v} -D l_table={l_table} -D c1={c1} -D c2={c2} -D l_coin={l_coin} -D l_mire={l_mire} -D mire={mire} --camera 13,-12,18,45.2,0,355.4,292 -o image1.png ../Openscad/BouchonMire.scad'
-os.system(chaine)
-print("openscad")
-exit()
-##################################
+def generer_openscad(d1,d2,mire,l_mire):
+    
+    #chemin d'accès à openscad à modifier
+    executableopenscad="/Applications/OpenSCAD-2021.01.app/Contents/MacOS/OpenSCAD"
+
+    l_table=100
+    u=random.uniform((l_table-l_mire)/2+d1/2,(l_table-l_mire)/2+l_mire-d1/2)
+    v=random.uniform((l_table-l_mire)/2+d1/2,(l_table-l_mire)/2+l_mire-d1/2)
+    print("U: ", u, "V: ", v)
+
+    c1="[109/255,119/255,160/255]"
+    c2="[249/255,58/255,58/255]"
+    chemin_scad = "./Openscad/BouchonMire2.scad"
+
+    # Liste de commandes et paramètres
+    commande = [
+        executableopenscad,
+        "-D", f"d1={d1}",
+        "-D", f"d2={d2}",
+        "-D", f"u={u}",
+        "-D", f"v={v}",
+        "-D", f"c1={c1}",
+        "-D", f"c2={c2}",
+        "-D", f"l_mire={l_mire}",
+        "-D", f"mire={mire}",
+        "--camera", "58,30,12,40,0,16,246",
+        "--imgsize", "1920,1080",
+        "-o", "imabouchon_openscad.png",
+        chemin_scad 
+    ]
+    print("Commande générée :", " ".join(commande))
+    subprocess.run(commande)
+    print("")
+
+
 
 def estimate_rigid_transform(p1_a, p2_a, p1_b, p2_b):
     v_a = np.array([p2_a[0] - p1_a[0], p2_a[1] - p1_a[1]])
@@ -43,6 +62,8 @@ def estimate_rigid_transform(p1_a, p2_a, p1_b, p2_b):
 
     return theta, translation[0], translation[1]
 
+
+
 def create_pattern_and_mask(width, d1, d2, bgr_color_center, bgr_color_contour):
 
     centre_cercle=(width/2,width/2)
@@ -62,12 +83,15 @@ def create_pattern_and_mask(width, d1, d2, bgr_color_center, bgr_color_contour):
     cv2.imwrite('src/images/template/template_bouchon.png', img_bouchon)
     cv2.imwrite('src/images/template/template_mask_bouchon.png', img_mask_bouchon)
 
+
+
 def drawcross(image,u,v):
-    cv2.line(image,(u-5,v),(u+5,v),(0,255,0),1)
-    cv2.line(image,(u,v-5),(u,v+5),(0,255,0),1)
+    cv2.line(image,(u-5,v),(u+5,v),(255,0,0),1)
+    cv2.line(image,(u,v-5),(u,v+5),(255,0,0),1)
+
+
 
 def processImage(image: str, srcPoints):
-
     if image is None:
         raise FileNotFoundError(f"Impossible de charger l'image : {image_path}")
 
@@ -75,84 +99,88 @@ def processImage(image: str, srcPoints):
     homography, _ = cv2.findHomography(srcPoints, target_points )
     transformed_image = cv2.warpPerspective(image, homography, (168, 168))
 
-    cv2.imwrite("transformed_image.png", transformed_image)
-    
+    # transformed_image = cv2.imread("src/images/bouchon/imabouchon2_rect.jpg")
+
     template = cv2.imread('src/images/template/template_bouchon.png')
     mask = cv2.imread('src/images/template/template_mask_bouchon.png')
-    # Obtenir les dimensions du modèle
     w, h,  = template.shape[:2]
-
-    # Appliquer le template matching
-    result = cv2.matchTemplate(transformed_image, template, cv2.TM_CCOEFF_NORMED, mask=mask)
-
-    # Trouver la position du maximum de la correspondance
+    result = cv2.matchTemplate(transformed_image, template, cv2.TM_CCORR_NORMED , mask=mask)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-    print(max_val)
+    print("Max_val: ", max_val)
+    print("")
 
-    if max_val>0.5:
-        # Dessiner un rectangle autour de la zone correspondante
+    if max_val>0.92:
         top_left = max_loc
-        top_right = (top_left[0] + w, top_left[1])
-        bottom_left = (top_left[0] , top_left[1] + h)
-        bottom_right = (top_left[0] + w, top_left[1] + h)
-        # cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 1)
-
-        center = (top_left[0] + w//2, top_left[1] + h//2)
-        # cv2.circle(transformed_image, center, 16, (0, 255, 0), 1)
-        # cv2.circle(transformed_image, center, 11, (0, 255, 0), 1)
-        drawcross(transformed_image,top_left[0],top_left[1])
-        drawcross(transformed_image,bottom_right[0],bottom_right[1])
-        drawcross(transformed_image,top_right[0],top_right[1])
-        drawcross(transformed_image,bottom_left[0],bottom_left[1])
-        cv2.line(transformed_image,top_left,top_right,(255,0,0),1)
-        cv2.line(transformed_image,top_left,bottom_left,(255,0,0),1)
-        cv2.line(transformed_image,top_right,bottom_right,(255,0,0),1)
-        cv2.line(transformed_image,bottom_left,bottom_right,(255,0,0),1)
+        center = (top_left[0] + w//2 , top_left[1] + h//2)
+        cv2.circle(transformed_image, center, 16, (0, 0, 255), 1)
+        cv2.circle(transformed_image, center, 11, (0, 255, 0), 1)
         drawcross(transformed_image,center[0],center[1])
+    else:
+        center = None
 
-    cv2.imshow('Template Matching', transformed_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.imwrite("imabouchon_openscad_rect_detection.png", transformed_image)
+    # cv2.imshow('Template Matching', transformed_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return center
 
 
-#Programme
+
+def afficher_coordonées(bouchon_mire):
+    if bouchon_mire!=None:
+        print("Le bouchon se trouve au coordonées x=", bouchon_mire[0], ", y=", bouchon_mire[1], " dans le repere mire.")
+        bouchon_mire = (80,80)
+        bouchon_robot = appliquer_transformatio_rigide(bouchon_mire, theta, translation_x, translation_y)
+        print("Le bouchon se trouve au coordonées x=", bouchon_robot[0], ", y=", bouchon_robot[1], " dans le repere robot.")
+    else:
+        print("Pas de bouchon detecté sur cette image")
+    print("")
+
+def appliquer_transformatio_rigide(pMire, theta, tx, ty):
+    pMire_x, pMire_y = pMire
+    pRobot_x = math.cos(math.radians(theta)) * pMire_x - math.sin(math.radians(theta)) * pMire_y + tx
+    pRobot_y = math.cos(math.radians(theta)) * pMire_x - math.sin(math.radians(theta)) * pMire_y + ty
+    return pRobot_x, pRobot_y
+
+
+
+#Programme Principal
+
+d1=15
+d2=10
+l_mire=80
+mire=0
+generer_openscad(d1,d2,mire,l_mire)
 
 with open('src/data/listecolors.out', 'r') as file:
-    # Créer une liste pour stocker les données
     couleurs = []
-    
-    # Lire chaque ligne du fichier
     for line in file:
-        # Supprimer les espaces ou caractères superflus et diviser la ligne par les virgules
         values = line.strip().split(',')
-        
-        # Convertir les valeurs en float (elles semblent être des nombres à virgule flottante)
         values = tuple(int(float(value.strip())) for value in values)
-        
-        # Ajouter les valeurs extraites à la liste data
         couleurs.append(values)
 
-with open('src/data/listepoints.out', 'r') as file:
-    points = []
-    
-    # Lire chaque ligne du fichier
-    for line in file:
-        # Supprimer les espaces et couper la ligne à la virgule
-        coordinates = line.strip().split(',')
-        
-        # Convertir les coordonnées en float et les ajouter à la liste points
-        x = float(coordinates[0].strip())
-        y = float(coordinates[1].strip())
-        points.append((x, y))
+#Choix de points correspondants dans le repere mire et robot 
+points = [(0,0), (80, 80), (345,526), (425,446)]
 
-# Afficher la liste des points
-print(points)
+print("P1 repère mire: ", points[0], "P2 repère mire: ", points[1], "\nP1  repère robot: ", points[2], "P2 repère robot: ", points[3])
+print("")
 
 theta, translation_x, translation_y = estimate_rigid_transform(points[0],points[1],points[2],points[3])
-print(f"theta: {theta} \n translation_x: {translation_x} \n translation_y: {translation_y}" )
+
+print(f"Theta: {theta} \nTranslation_x: {translation_x} \nTranslation_y: {translation_y}" )
+print("")
 
 create_pattern_and_mask(32,32,22,couleurs[1],couleurs[0])
-pixels_mire_source = np.array([[991, 564], [655, 489], [593, 831], [949, 900]], dtype=np.float32)
-image = cv2.imread("src/images/bouchon/imabouchon1.jpg")
-processImage(image, pixels_mire_source)
+
+#COORDONEES COIN MIRE EXEMPLE INETDOC
+# pixels_mire_source = np.array([[991, 564], [655, 489], [593, 831], [949, 900]], dtype=np.float32)
+# image = cv2.imread("src/images/bouchon/imabouchon1.jpg")
+
+#COORDONEES COIN MIRE EXEMPLE OPENSCAD
+pixels_mire_source = np.array([[1409,245], [691, 106], [399,659], [1250,860]], dtype=np.float32)
+image = cv2.imread("imabouchon_openscad.png")
+
+
+bouchon_mire = processImage(image, pixels_mire_source)
+afficher_coordonées(bouchon_mire)
